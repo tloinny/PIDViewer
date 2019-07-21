3fbdf97ad925416f9ae082bb532de941f4ec6f93
tloinny <tloinny@gmail.com>
2019-07-21 19:26:14 +0800

    debug

--- src/pid_viewer/pid_viewer.ino
+++ src/pid_viewer/pid_viewer.ino
@@ -1,13 +1,14 @@
+#include <FlexiTimer2.h>
 #include <easy_pid.h>
 #include "AS5600.h"
-#include <Servo.h>
 #include <Wire.h>
 
 AMS_5600 ams_5600;
-Servo motor;
-PID_CONTROLLER PID_contorller(0.2, 0, 0, 0.01, 20.0);
+PID_CONTROLLER PID_contorller(1000000, 0, 0, 0.001, 10.0);
 float feedback = 0;
 float output = 0;
+int throttle = 0;
+int get_p = 0;
 
 /**
  *@Function: convertScaledAngleToDegrees
@@ -46,18 +47,53 @@ float convertScaledAngleToDegrees(word newAngle)
   return (newAngle * multipler);
 }
 
+void Event()
+{
+  digitalWrite(3,HIGH);
+  delayMicroseconds(throttle);	/* The stepper motor will be faster if this value become smaller */
+  digitalWrite(3,LOW);
+}
+
 void setup()
 {
-	Serial.begin(115200);
+	FlexiTimer2::set(3, 1.0/10000, Event);
+	pinMode(3, OUTPUT);
+	pinMode(4, OUTPUT);
+	FlexiTimer2::start();
 	Wire.begin();
-	ams_5600.setStartPosition(word(134.15399/0.087));
-	ams_5600.setEndPosition(word(324.94498/0.087));
-	PID_contorller.setGoal(90);
+	Serial.begin(115200);
+	ams_5600.setStartPosition(word(0/0.087));
+	ams_5600.setEndPosition(word(355/0.087));
+	PID_contorller.setGoal(180);
+	Serial.println("ready");
+  throttle=1000;
 }
 
 void loop()
 {
+  Serial.print("angle: ");
+  Serial.print(convertScaledAngleToDegrees(ams_5600.getScaledAngle()), DEC);
+  Serial.print("  ");
 	feedback = convertScaledAngleToDegrees(ams_5600.getScaledAngle());
-	output = PID_contorller.update(feedback);
-	Serial.println(output, DEC);
+	output = PID_contorller.update(feedback);	/* Stepper motor should run faster if this value become lager */
+	Serial.print("output: ");
+	Serial.print(output, DEC);
+	Serial.println("  ");
+    if(output > 0.1)
+    {
+      FlexiTimer2::start();
+		  digitalWrite(4, HIGH);
+		  //throttle = map(output*100, 0, 100, 0, 9999);
+		  throttle -= output;
+    }else if(output < -0.1)
+    {
+      FlexiTimer2::start();
+		  digitalWrite(4, LOW);
+		  //throttle = map(output*100, -100, 0, -3, 9999);
+		  throttle -= output;
+    }
+    else
+    {
+		  FlexiTimer2::stop();
+    }
 }
